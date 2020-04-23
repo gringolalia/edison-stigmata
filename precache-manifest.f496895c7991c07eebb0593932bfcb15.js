47fa0c3bea4f50cd25e5885bee62be0fed38381f
Colin Brayton <gringolalia@gmail.com>
2020-04-23 11:53:32 -0300

    deploy

---
+++ precache-manifest.f496895c7991c07eebb0593932bfcb15.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "65202749bd98379a7d2d",
+    "url": "/edison-stigmata/js/about.20f539e8.js"
+  },
+  {
+    "revision": "6a0fbcff8af9b9569bbe",
+    "url": "/edison-stigmata/css/app.1f9f02cb.css"
+  },
+  {
+    "revision": "6a0fbcff8af9b9569bbe",
+    "url": "/edison-stigmata/js/app.b097c825.js"
+  },
+  {
+    "revision": "0005f563a64e56cbc3a6",
+    "url": "/edison-stigmata/css/chunk-vendors.b8edc7ed.css"
+  },
+  {
+    "revision": "0005f563a64e56cbc3a6",
+    "url": "/edison-stigmata/js/chunk-vendors.776e6e59.js"
+  },
+  {
+    "revision": "3fea563c5316b0039389b59f0a876105",
+    "url": "/edison-stigmata/img/hammer-sickle.3fea563c.svg"
+  },
+  {
+    "revision": "bc2d18b25f99b66f2a3225f235656d87",
+    "url": "/edison-stigmata/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/edison-stigmata/robots.txt"
+  }
+];
